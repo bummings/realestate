1d2860b5dfd1a9796beea75e312bfd2e9aa66040
fauxkno <sup@fauxkno.com>
2018-03-31 18:34:09 -0400

    begin working on click handlers

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var user1 = document.getElementById('user1');
+var user2 = document.getElementById('user2');
+var user3 = document.getElementById('user3');
+var quoteBox = document.getElementById('quoteBox');
+
+user1.addEventListener('click', function(){
+	quoteBox.innerHTML = 'bruh';
+});
