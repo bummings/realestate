8c297d379f72aaf9b264e9ac2387a386107de37c
fauxkno <sup@fauxkno.com>
2018-04-02 14:26:59 -0400

    fix mouseover eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,44 +1,71 @@
-var user1 = document.getElementById('user1');
-var user2 = document.getElementById('user2');
-var user3 = document.getElementById('user3');
+var user1 	 = document.getElementById('user1');
+var user2 	 = document.getElementById('user2');
+var user3 	 = document.getElementById('user3');
 var quoteBox = document.getElementById('quoteBox');
 var userName = document.getElementById('userName');
 var userCity = document.getElementById('userCity');
 
-user1.addEventListener('click', function(){
+user1.addEventListener('mouseover', function(){
 	
-	user1.classList.remove('feedback--images__image__unselected');	
+	user1.classList.remove('feedback--images__image__unselected');
+	user1.classList.add('feedback--images__image__userSelect');
+
 	user2.classList.add('feedback--images__image__unselected');
 	user3.classList.add('feedback--images__image__unselected');
 
+	quoteBox.classList.add('feedback--quote__quoteFade');
 	quoteBox.innerHTML = "I laugh at all my friends who live in other neighborhoods. They should use Gentrifi!";
-	username.innerHTML = "Emily Ackerman";
+	userName.innerHTML = "Emily Ackerman";
 	userCity.innerHTML = "Mt. Airy"
-
 });
 
-user2.addEventListener('click', function(){
+user1.addEventListener('mouseout', function(){
+	user1.classList.remove('feedback--images__image__userSelect');
+	user1.classList.add('feedback--images__image__unselected');
+});
+
+
+
+
+
+user2.addEventListener('mouseover', function(){
 	user1.classList.add('feedback--images__image__unselected');	
 	user2.classList.remove('feedback--images__image__unselected');
+	user2.classList.add('feedback--images__image__userSelect');
 	user3.classList.add('feedback--images__image__unselected');
 	
+	quoteBox.classList.add('feedback--quote__quoteFade');
 	quoteBox.innerHTML = "I couldn't believe how easy it was to find a siiick apartment in a hip neighborhood like Fishtown.";
 
-	username.innerHTML = "Ethan Sherman";
+	userName.innerHTML = "Ethan Sherman";
 	userCity.innerHTML = "Fishtown";
-
-
 });
 
-user3.addEventListener('click', function(){
+user2.addEventListener('mouseout', function(){
+	user2.classList.remove('feedback--images__image__userSelect');
+	user2.classList.add('feedback--images__image__unselected');
+});
+
+
+
+
+
+user3.addEventListener('mouseover', function(){
 	user1.classList.add('feedback--images__image__unselected');	
 	user2.classList.add('feedback--images__image__unselected');
 	user3.classList.remove('feedback--images__image__unselected');
+	user3.classList.add('feedback--images__image__userSelect');
 	
+	quoteBox.classList.add('feedback--quote__quoteFade');
 	quoteBox.innerHTML = "lol what am I doing, I can't afford these places";
 
-	username.innerHTML = "Pavlov Lawrence";
+	userName.innerHTML = "Pavlov Lawrence";
 	userCity.innerHTML = "Collingswood";
+});
+
+user3.addEventListener('mouseout', function(){
+	user3.classList.remove('feedback--images__image__userSelect');
+	user3.classList.add('feedback--images__image__unselected');
 });
 
 
